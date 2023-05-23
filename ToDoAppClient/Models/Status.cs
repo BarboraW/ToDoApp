eecd8df2bcd5aaa7a7ae777b32446481283430ba
Barbora Winklerová <barbora.winkler@gmail.com>
2023-05-23 14:27:28 +0200

    Models Status.cs and TaskModel.cs added

---
+++ ToDoAppClient/Models/Status.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoAppClient.Models
+{
+    /// <summary>
+    /// Represents the status of a task.
+    /// </summary>
+    public enum Status
+    {
+        /// <summary>
+        /// The task has not started.
+        /// </summary>
+        NotStarted = 0,
+
+        /// <summary>
+        /// The task is in progress.
+        /// </summary>
+        InProgress = 1,
+
+        /// <summary>
+        /// The task has been completed.
+        /// </summary>
+        Completed = 2,
+    }
+}
---
+++ ToDoAppClient/Models/TaskModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAppClient.Models
+{
+    /// <summary>
+    /// Represents a model for a task.
+    /// </summary>
+    public class TaskModel
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the task.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the task.
+        /// </summary>
+        [Required(ErrorMessage = "The Name field is required.")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the priority of the task.
+        /// </summary>
+        [Required(ErrorMessage = "The Priority field is required.")]
+        [Range(1,10, ErrorMessage = "Enter a value between 1 and 10")]
+        public int Priority { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the task.
+        /// </summary>
+        [Required(ErrorMessage = "The Status field is required.")]
+        public Status Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of creation of the task.
+        /// </summary>
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
