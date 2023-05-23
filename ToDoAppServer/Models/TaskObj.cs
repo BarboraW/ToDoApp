9dc2e18f31cba3a85f3bb80902148f60837a3723
Barbora Winklerová <barbora.winkler@gmail.com>
2023-05-23 13:58:31 +0200

    Models TaskObj.cs and Status.cs added to ToDoAppServer

---
+++ ToDoAppServer/Models/TaskObj.cs
@@ -0,0 +1,33 @@
+﻿namespace ToDoAppServer.Models
+{
+    /// <summary>
+    /// Represents a task object.
+    /// </summary>
+    public record TaskObj
+    {
+        /// <summary>
+        /// Gets or sets the unique identifier of the task.
+        /// </summary>
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the task.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the priority of the task.
+        /// </summary>
+        public int Priority { get; set; }
+
+        /// <summary>
+        /// Gets or sets the status of the task.
+        /// </summary>
+        public Status Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of creation of the task.
+        /// </summary>
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
